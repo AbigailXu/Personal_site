c09f376dab3104aadcb3733f4374f315db9578ad
AbigailXu <qbxzz233@gmail.com>
2021-06-18 16:30:32 -0400

    add to p2

--- src/components/page2.jsx
+++ src/components/page2.jsx
@@ -8,10 +8,10 @@ class Page2 extends Component {
       <Fragment>
         <h1 className="p2-title">WELCOME!</h1>
         <h4 className="p2-sub">to Abi's personal website</h4>
-        <div class="container page2-links">
-          <div class="row">
-            <div class="col col-lg-4 col-12">
-              <a class="page2-link-inner" href="/about">
+        <div className="container page2-links">
+          <div className="row">
+            <div className="col col-lg-4 col-12">
+              <a className="page2-link-inner" href="/about">
                 <FontAwesomeIcon
                   className="p2-icon-2"
                   icon={["far", "user-circle"]}
@@ -19,8 +19,8 @@ class Page2 extends Component {
                 <h5 className="p2-sub">ABOUT ME</h5>
               </a>
             </div>
-            <div class="col col-lg-4 col-12">
-              <a class="page2-link-inner" href="/projects">
+            <div className="col col-lg-4 col-12">
+              <a className="page2-link-inner" href="/projects">
                 <FontAwesomeIcon
                   className="p2-icon-2"
                   icon={["fas", "project-diagram"]}
@@ -28,8 +28,8 @@ class Page2 extends Component {
                 <h5 className="p2-sub">MY PROJECTS</h5>
               </a>
             </div>
-            <div class="col col-lg-4 col-12">
-              <a class="page2-link-inner" href="/">
+            <div className="col col-lg-4 col-12">
+              <a className="page2-link-inner" href="/">
                 <FontAwesomeIcon
                   className="p2-icon-2"
                   icon={["far", "comments"]}

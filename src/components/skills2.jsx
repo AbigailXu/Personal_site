d6b7cfc0eb8077b303c5d4eee6c8209335a82ecd
AbigailXu <qbxzz233@gmail.com>
2021-06-18 00:38:52 -0400

    fix unused

--- src/components/skills2.jsx
+++ src/components/skills2.jsx
@@ -4,7 +4,7 @@ import "aos/dist/aos.css";
 
 import "../styles/skills.css";
 import { useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const Skill = ({ skill, img, dir}) => {
